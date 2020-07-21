af6dc21f09738b15620e0c8082129db50b067cf7
Shubham Singh <shubham88singh@gmail.com>
2020-07-21 21:26:31 +0530

    Addingt the animation to the player

--- src/index.js
+++ src/index.js
@@ -4,8 +4,14 @@ import logoImg from './assets/logo.png';
 const config = {
   type: Phaser.AUTO,
   parent: 'phaser-example',
-  width: 800,
+  width: 1200,
   height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 0 },
+    },
+  },
   scene: {
     preload: preload,
     create: create,
@@ -19,7 +25,12 @@ function preload() {
   this.load.image('tiles', '../src/assets/assets.png');
   this.load.tilemapTiledJSON('map', '../src/assets/level1.json');
   this.load.image('background', '../src/assets/water.png');
+  this.load.spritesheet('player', '../src/assets/player.png', {
+    frameWidth: 32,
+    frameHeight: 64,
+  });
 }
+let player, cursors;
 
 function create() {
   const map = this.make.tilemap({ key: 'map' });
@@ -31,6 +42,67 @@ function create() {
   const grassLayer = map.createStaticLayer('Grass', tileset, 0, 0);
   const worldLayer = map.createStaticLayer('World', tileset, 0, 0);
   const highLayer = map.createStaticLayer('High', tileset, 0, 0);
+  player = this.physics.add.sprite(100, 450, 'player');
+  const anims = this.anims;
+  anims.create({
+    key: 'left',
+    frames: anims.generateFrameNames('player', {
+      start: 20,
+      end: 29,
+      frames: 10,
+      repeat: -1,
+    }),
+  });
+
+  anims.create({
+    key: 'right',
+    frames: anims.generateFrameNames('player', {
+      start: 30,
+      end: 39,
+      frames: 10,
+      repeat: -1,
+    }),
+  });
+  anims.create({
+    key: 'front',
+    frames: anims.generateFrameNames('player', {
+      start: 0,
+      end: 7,
+      frames: 10,
+      repeat: -1,
+    }),
+  });
+  anims.create({
+    key: 'back',
+    frames: anims.generateFrameNames('player', {
+      start: 10,
+      end: 17,
+      frames: 10,
+      repeat: -1,
+    }),
+  });
 }
 
-function update() {}
+function update() {
+  player.body.setVelocity(0);
+  cursors = this.input.keyboard.createCursorKeys();
+  if (cursors.left.isDown) {
+    player.body.setVelocityX(-100);
+  } else if (cursors.right.isDown) {
+    player.body.setVelocityX(100);
+  } else if (cursors.up.isDown) {
+    player.body.setVelocityY(-100);
+  } else if (cursors.down.isDown) {
+    player.body.setVelocityY(100);
+  }
+
+  if (cursors.left.isDown) {
+    player.anims.play('left', true);
+  } else if (cursors.right.isDown) {
+    player.anims.play('right', true);
+  } else if (cursors.up.isDown) {
+    player.anims.play('back', true);
+  } else if (cursors.down.isDown) {
+    player.anims.play('front', true);
+  } else player.anims.stop();
+}
